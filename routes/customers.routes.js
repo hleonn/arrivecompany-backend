34abd4f87b97123c4647a07cdc1ddba5cb4a0ca6
hleonn <edhleon2001@hotmail.com>
2022-08-02 22:04:32 -0500

    qr test-routes

--- routes/customers.routes.js
+++ routes/customers.routes.js
@@ -1,39 +1,54 @@
-const express = require ("express")
+const express = require("express")
 const router = express.Router()
-const Customer =require("../models/Customer.model")
+const Customer = require("../models/Customer.model")
 
 
 //vamos a agregar las rutas
 //http://localhost:5005/api/customers
 //Todos los customers
-router.get("/", async (req,res) =>{
-    try{
-    const customers = await Customer.find()    
-    res.json(customers);
-}catch(err){
-    console.log(err)
-}
+router.get("/", async (req, res) => {
+    try {
+        const customers = await Customer.find()
+        res.json(customers);
+    } catch (err) {
+        console.log(err)
+    }
 });
+
+
+
+//vamos a agregar las rutas
+//http://localhost:5005/api/customers/{ID}
+//Todos los customers
+router.get("/:id", async (req, res) => {
+    try {
+        const customer = await Customer.findOne({ _id: req.params.id })
+        res.json(customer);
+    } catch (err) {
+        console.log(err)
+    }
+});
+
 
 //POST http://localhost:5005/api/customers
 //Crear Customer y recibir los datos en req.body... meter los datos en una box
-router.post("/",(req,res)=>{
-    const {body} = req;
+router.post("/", (req, res) => {
+    const { body } = req;
 
     console.log(body);
 
     Customer.create(body)
-    .then (resultado =>{
-    res.json(resultado)
-    }).catch(function (error) {
-        console.log(error)
-        res.status(500).json({
-            error: {
-                message: error
-            }
+        .then(resultado => {
+            res.json(resultado)
+        }).catch(function (error) {
+            console.log(error)
+            res.status(500).json({
+                error: {
+                    message: error
+                }
+            })
         })
-    })
-    
+
 });
 
 module.exports = router;
